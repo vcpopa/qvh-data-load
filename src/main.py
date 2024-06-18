a0eab7f31ab8f5762351b8c03ac2fe23a5d4df20
vcpopa <victorpopa11@gmail.com>
2024-06-18 12:52:04 +0100

    elective recovery

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ from fileshare import (
     download_from_fileshare,
     archive_in_fileshare,
 )
-from sql import connection, merge_data, log_file
+from sql import connection, merge_data, log_file,execute_query
 from utils import make_dir, generate_id, filter_files, process_file
 
 
@@ -43,56 +43,56 @@ if __name__ == "__main__":
     client_id = os.environ["AZURE_CLIENT_ID"]
     client_secret = os.environ["AZURE_CLIENT_SECRET"]
     tenant_id = os.environ["AZURE_TENANT_ID"]
-    # files = list_files_in_blob_storage(container_name="qvh")
-    # files = [file for file in files if "Processed" not in file]
-    # files = filter_files(files)
-    # print(files)
-    # if files != []:
-    #     previous_data = None
-    #     run_id = generate_id()
-    #     make_dir(run_id)
-    #     for file in files:
-    #         file_name = file.split("/")[-1]
-    #         download_file_from_blob_storage(
-    #             container_name="qvh",
-    #             blob_name=file,
-    #             download_path=f"./{run_id}/{file_name}",
-    #         )
-    #         archive_path = f"home/IQPR/Processed/{file_name}"
-    #         archive_in_blob(
-    #             container_name="qvh", src_blob_name=file, dest_blob_name=archive_path
-    #         )
-    #         data = process_file(f"./{run_id}/{file_name}")
-    #         data = data.reset_index(drop=False).drop("Denominator", axis=1)
-    #         data.columns = [
-    #             "Metric Name",
-    #             "Period",
-    #             "Specialty/Trust",
-    #             "Numerator",
-    #             "Denominator",
-    #             "SourceFile",
-    #         ]
+    files = list_files_in_blob_storage(container_name="qvh")
+    files = [file for file in files if "Processed" not in file]
+    files = filter_files(files)
+    print(files)
+    if files != []:
+        previous_data = None
+        run_id = generate_id()
+        make_dir(run_id)
+        for file in files:
+            file_name = file.split("/")[-1]
+            download_file_from_blob_storage(
+                container_name="qvh",
+                blob_name=file,
+                download_path=f"./{run_id}/{file_name}",
+            )
+            archive_path = f"home/IQPR/Processed/{file_name}"
+            archive_in_blob(
+                container_name="qvh", src_blob_name=file, dest_blob_name=archive_path
+            )
+            data = process_file(f"./{run_id}/{file_name}")
+            data = data.reset_index(drop=False).drop("Denominator", axis=1)
+            data.columns = [
+                "Metric Name",
+                "Period",
+                "Specialty/Trust",
+                "Numerator",
+                "Denominator",
+                "SourceFile",
+            ]
 
-    #         if previous_data is not None and previous_data.equals(data):
-    #             print(
-    #                 f"File '{file_name}' is identical to the previous file. Skipping SQL write."
-    #             )
-    #         else:
-    #             with connection() as conn:
-    #                 assert "Metric Name" in data.columns, "Metric name col is missing"
-    #                 data.to_sql(
-    #                     name="Metrics_Generic",
-    #                     con=conn,
-    #                     schema="staging",
-    #                     if_exists="replace",
-    #                     index=False,
-    #                 )
-    #                 merge_data(source="staging.Metrics_Generic", target="scd.Metric")
-    #                 log_file(file_name=file_name, source="SFTP")
-    #                 previous_data = data
+            if previous_data is not None and previous_data.equals(data):
+                print(
+                    f"File '{file_name}' is identical to the previous file. Skipping SQL write."
+                )
+            else:
+                with connection() as conn:
+                    assert "Metric Name" in data.columns, "Metric name col is missing"
+                    data.to_sql(
+                        name="Metrics_Generic",
+                        con=conn,
+                        schema="staging",
+                        if_exists="replace",
+                        index=False,
+                    )
+                    merge_data(source="staging.Metrics_Generic", target="scd.Metric")
+                    log_file(file_name=file_name, source="SFTP")
+                    previous_data = data
 
-    # else:
-    #     print("No new files,skipping...")
+    else:
+        print("No new files,skipping...")
 
     files = list_files_in_fileshare(fileshare_name="qvh", directory_path="Uploads/IQPR")
     files=filter_files(files=files)
@@ -122,18 +122,55 @@ if __name__ == "__main__":
             else:
                 with connection() as conn:
                     data.to_sql(
-                        name="Metrics_Generic",
+                        name="Metrics_ElectiveRecovery",
                         con=conn,
                         schema="staging",
                         if_exists="replace",
                         index=False,
                     )
-                merge_data(source="staging.Metrics_Generic", target="scd.Metric")
+                execute_query(source="staging.Metrics_Generic", target="scd.Metric")
                 log_file(file_name=file_name, source="SFTP")
                 previous_data = data
     else:
         print("No files,skipping...")
 
+
     files = list_files_in_fileshare(fileshare_name="qvh", directory_path="Uploads/IQPR/ElectiveRecovery")
     files=filter_files(files=files)
     print(files)
+    if files != []:
+        previous_data = None
+        run_id = generate_id()
+        make_dir(run_id)
+        for file in files:
+            print(f"Processing {file}")
+            file_name = file.split("/")[-1]
+            download_from_fileshare(
+                fileshare_name="qvh",
+                fileshare_path=f"Uploads/IQPR/{file}",
+                local_file_path=f"./{run_id}/{file_name}",
+            )
+            # archive_in_fileshare(
+            #     fileshare_name="qvh",
+            #     source_path=f"Uploads/IQPR/ElectiveRecovery/{file}",
+            #     destination_path=f"Uploads/IQPR/Processed/{file}",
+            # )
+            data = process_file(f"./{run_id}/{file_name}")
+            if previous_data is not None and previous_data.equals(data):
+                print(
+                    f"File '{file_name}' is identical to the previous file. Skipping SQL write."
+                )
+            else:
+                with connection() as conn:
+                    data.to_sql(
+                        name="Metrics_ElectiveRecovery",
+                        con=conn,
+                        schema="staging",
+                        if_exists="replace",
+                        index=False,
+                    )
+                # merge_data(source="staging.Metrics_Generic", target="scd.Metric")
+                # log_file(file_name=file_name, source="FileShare")
+                previous_data = data
+    else:
+        print("No files,skipping...")
