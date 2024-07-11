e9de0032762b1f3f6fe8b18a6be9c055bee00c91
vcpopa <81989171+vcpopa@users.noreply.github.com>
2024-07-11 14:15:19 +0100

    remove reset_index

--- src/main.py
+++ src/main.py
@@ -63,7 +63,6 @@ if __name__ == "__main__":
 
             data = process_file(f"./{run_id}/{file_name}")
 
-            data = data.reset_index(drop=False)
             required_columns = {'Metric Name', 'Period', 'Specialty/Trust', 'Numerator', 'Denominator'}
             if not required_columns.issubset(set(data.columns)):
                 print("Data does not match the headers, skipping")
